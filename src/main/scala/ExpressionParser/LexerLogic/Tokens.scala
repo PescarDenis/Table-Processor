c0315ccecec2cf9f94687906e8697da3bc9932c4
pescaden <pescaden@student.cvut.cz>
2024-11-23 14:28:08 +0100

    Modified working flow + comments

--- src/main/scala/ExpressionParser/LexerLogic/Tokens.scala
+++ src/main/scala/ExpressionParser/LexerLogic/Tokens.scala
@@ -1,10 +1,12 @@
 package ExpressionParser.LexerLogic
 
 
-sealed trait Tokens //base type for all tokens
+sealed trait Tokens //the sealed trait is used as an Enum in scala
 case class NumberToken(value : String) extends Tokens //represents a number found in the formula
 case class OperatorToken(op : String) extends Tokens //represents the operators used in the formula...
 case class RefToken(ref : String) extends Tokens //represents a cell in the table D20 or so...
+case object EndOfInputToken extends Tokens //represents the end of the token
+
 
 
 
